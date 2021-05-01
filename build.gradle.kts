05c07d3332b426259525565ff5c6bd141ff8a2d4
Michael Akinde <michael.akinde@statnett.no>
2021-05-01 02:00:38 +0200

    Fix the dependency on common-bom

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,14 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("no.elhub.common:common-bom:1.0-SNAPSHOT"))
+    implementation(platform("no.elhub.common:common-bom:0.1.0"))
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     // Kotlin Platform
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     // Test Dependencies
-    testImplementation("io.kotest:kotest-runner-junit5:4.4.3")
-    testImplementation("io.kotest:kotest-extensions-allure-jvm:4.4.3")
-    testImplementation("org.slf4j:slf4j-simple:1.7.30")
+    testImplementation("io.kotest:kotest-runner-junit5")
+    testImplementation("io.kotest:kotest-extensions-allure-jvm")
+    testImplementation("org.slf4j:slf4j-simple")
 }
 
 /*
